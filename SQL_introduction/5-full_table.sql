afde4ffad39637b5f0f0a1982d0955f295e5ca64
Thembelihle <thembelihlengobese3@gmail.com>
2023-08-16 19:47:59 +0200

    description

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,4 +1,2 @@
 -- This is a script that prints the full description of the table
--- SHOW COLUMNS FROM first_table;
-SHOW * FROM information_schema.columns 
-WHERE table_name = 'first_table' AND table_schema = 'hbtn_0c_0';
+SHOW COLUMNS FROM first_table;
