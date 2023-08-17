6765aeeb1a59a97bf1c4741cc2f3d7a7bf8ea183
Thembelihle <thembelihlengobese3@gmail.com>
2023-08-17 23:14:51 +0200

    cities

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -5,5 +5,6 @@ hbtn_0d_usa) on your MySQL server.*/
 CREATE database IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities(
     id int NOT NULL UNIQUE PRIMARY KEY,
-    state_id int NOT NULL FOREIGN KEY(state_id) REFERENCES (states)
+    state_id int NOT NULL FOREIGN KEY(state_id) REFERENCES (states),
+    name varchar(256) NOT NULL
 );
