5707a218225d54a16992650d409b6c9a264232f1
Thembelihle <thembelihlengobese3@gmail.com>
2023-08-17 23:04:19 +0200

    user

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -2,4 +2,5 @@
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
 SET PASSWORD FOR 'user_0d_1'@'localhost' = 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES
-ON MySQL.* TO user_0d_1@localhost;
+ON *.* 
+TO user_0d_1@localhost;
