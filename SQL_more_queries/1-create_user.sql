9fb562d4ee02768418297727dfa9faca6269a033
Thembelihle <thembelihlengobese3@gmail.com>
2023-08-17 20:49:13 +0200

    user

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- this is a script that creates the MySQL server user
-CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL 
 ON  MySQL.*
 TO user_0d_1@localhost
