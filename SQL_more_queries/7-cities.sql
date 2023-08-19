2521128fe6d33e09c7620426c1fc5c0b2cb50649
Thembelihle <thembelihlengobese3@gmail.com>
2023-08-19 15:29:50 +0200

    cities

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,6 +1,4 @@
-/*Write a script that creates the database 
-hbtn_0d_usa and the table cities (in the database
-hbtn_0d_usa) on your MySQL server.*/
+-- a script that creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) on your MySQL server.
 
 CREATE database IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities(
