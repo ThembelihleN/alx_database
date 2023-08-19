842088675a08169aca3cd3d0cbc0ed9bd83a7477
Thembelihle <thembelihlengobese3@gmail.com>
2023-08-19 15:51:41 +0200

    carlifonia

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,5 @@
 -- a script that lists all the cities of California that can be found in the database hbtn_0d_usa.
 
-SHOW table FROM hbtn_0d_usa;
-SELECT * FROM cities
+SHOW table
+FROM hbtn_0d_usa;
+SELECT * FROM cities;
