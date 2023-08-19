dfcffb208dac7ab91f5217843f755bd4a59fb890
Thembelihle <thembelihlengobese3@gmail.com>
2023-08-19 12:22:55 +0200

    read user

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,7 @@
 -- this is a a script that creates the database and the user
 CREATE database IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
+SET PASSWORD FOR 'user_0d_2'@'localhost' = 'user_0d_2_pwd'; 
 GRANT SELECT 
 ON hbtn_0d_2
 TO user_0d_2;
