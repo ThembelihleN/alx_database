10c17a4912a33429ab8573c39c95bde678988244
Thembelihle <thembelihlengobese3@gmail.com>
2023-08-19 13:12:20 +0200

    never empty

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,5 +1,4 @@
-/*this is a script that creates the table 
-with id that can't be null on my server*/
+-- this is a script that creates the table with id that can't be null on my server
 
 CREATE TABLE IF NOT EXISTS id_not_null(
     id int NOT NULL (1),
