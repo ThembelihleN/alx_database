0f2a12807eff98b87e5825420963cdcd239a74fe
Thembelihle <thembelihlengobese3@gmail.com>
2023-08-19 15:20:47 +0200

    state

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
  -- this is a script that creates the database and the table states on your MySQL server.
 
 CREATE database IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states(
+CREATE TABLE IF NOT EXISTS states (
     id int UNIQUE
-)
+);
