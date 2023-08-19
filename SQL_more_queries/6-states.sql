bd5a53f23b79bdd34b1f76e3975294dee1834587
Thembelihle <thembelihlengobese3@gmail.com>
2023-08-19 13:16:15 +0200

    states

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,6 +1,4 @@
-/*Write a script that creates the database hbtn_0d_usa
-and the table states (in the database hbtn_0d_usa)
-on your MySQL server.*/
+ --Write a script that creates the database hbtn_0d_usaand the table states (in the database hbtn_0d_usa) on your MySQL server.
 
 CREATE database IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states(
