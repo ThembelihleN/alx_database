afc0d4309aeb91a119e450151758b5d12f881a05
Thembelihle <thembelihlengobese3@gmail.com>
2023-08-22 11:42:53 +0200

    seven cities

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS cities(
     id int UNIQUE PRIMARY KEY NOT NULL,
     state_id int FOREIGN KEY(state_id) REFERENCES (states) NOT NULL,
     name varchar(256) NOT NULL
-);*/
+);
 
 
 CREATE database IF NOT EXISTS hbtn_0d_usa;
@@ -16,4 +16,12 @@ CREATE TABLE IF NOT EXISTS cities(
     state_id int,
     FOREIGN KEY(state_id) REFERENCES states(id),
     name varchar(256) NOT NULL
+);*/
+
+CREATE database IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (
+    id INT PRIMARY KEY NOT NULL,
+    state_id INT REFERENCES states(id),
+    name VARCHAR(256) NOT NULL
 );
